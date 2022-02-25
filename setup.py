eb4e99f3ae8b3df2cb4c26941ef53a4340e21fe7
唐沢 一成 <kazushige-karasawa@tenryuseiki.com>
2022-02-25 16:04:13 +0900

    分類のモデルを評価を追加

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "utility"
-    , version = "0.0.1"
+    , version = "0.0.2"
     , description = "Python用のライブラリ"
     , long_description = "共通して使用できる関数群"
     , author = "kazu.kara"
--- utility/evaluation/model.py
+++ utility/evaluation/model.py
@@ -1,11 +1,22 @@
+import numpy
 import sklearn
-from sklearn.preprocessing import LabelEncoder
+from sklearn import model_selection
 from sklearn import utils
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_validate
 
 # 分類のモデルを評価
 def model_classifier(
-    x, y
+    x
+    , y
+    , max_iter = 1000
+    , early_stopping = 10
+    , scorings = ["neg_log_loss"]
+    , cv = StratifiedKFold(shuffle = True)
 ):
+
+    results = {}
 
     # 目的変数をラベル化
     encoder = LabelEncoder()
@@ -19,48 +30,60 @@ def model_classifier(
     estimators.append(("lightgbm", lightgbm.LGBMClassifier))
     estimators.append(("catboost", catboost.CatBoostCalssifier))
 
+    # 早期打ち切りを設定
+    lightgbm.early_stopping(early_stopping)
+
     for (name, estimator) in estimators:
         try:
             model = None
-            if name == "HistGradientBoostingClassifier" or name == "GaussianProcessClassifier" or name == "MultinomialNB":
-                model = estimator(max_iter = 200)
+            if signature.parameters.get("max_iter") != None:
+                if signature.parameters.get("early_stopping") != None:
+                    model = estimator(max_iter = max_iter, early_stopping = early_stopping)
+
+                else:
+                    model = estimator(max_iter = max_iter)
+
             elif name == "xgboost":
                 model = estimator(objective = "binary:logistic")
+
             else:
                 model = estimator()
 
             if "score" not in dir(model):
-                print(f"{name},not score")
+                print(f"{name},not score", end = "")
                 continue
 
             fit_params = None
             if name == "xgboost" or name == "lightgbm" or name == "catboost":
                 fit_params = {}
                 fit_params["eval_set"] = [(x_train, label_train)]
+                fit_params["verbose"] = 0
 
                 if name == "xgboost":
-                    fit_params["early_stopping_rounds"] = 10
+                    fit_params["early_stopping_rounds"] = early_stopping
                     fit_params["eval_metric"] = "logloss"
 
                 elif name == "lightgbm":
                     fit_params["eval_metric"] = "multi_logloss"
 
             # クロスバリデーションで評価指標を算出
-            scores = sklearn.model_selection.cross_val_score(model
-                                                             , X = x_train, y = label_train
-                                                             , scoring = _scoring
-                                                             , cv = _cv
-                                                             , n_jobs = -1
-                                                             , fit_params = fit_params)
-            score = numpy.mean(scores)
-            print(f"{name},スコア:{scores},平均:{score}")
-            if target:
-                if maximum < score:
-                    target = name
-                    maximum= score
-            else:
-                target = name
-                maximum = score
+            scores = cross_validate(model
+                                    , X = x_train, y = label_train
+                                    , scoring = scorings
+                                    , cv = cv
+                                    , n_jobs = -1
+                                    , fit_params = fit_params)
+            results[name] = scores
+
+            message = name
+            for scoring in scorings:
+                target = "test_" + scoring
+                message += "," + scoring + ":"
+                message += " ".join([format(score, ".6f") for score in scores[target]])
+                message += ",平均 {:.6f}".format(scores[target].mean())
+            print(message, end = "")
 
         except Exception as e:
-            print(f"{name},エラー:{e}")
+            print(f"{name},エラー:{e}", end = "")
+
+    return results
