62c497c4fe492779561bcc73b9dab20425532b5b
唐沢 一成 <kazushige-karasawa@tenryuseiki.com>
2022-03-15 15:36:55 +0900

    予測値の算出を修正

--- utility/predict/__init__.py
+++ utility/predict/__init__.py
@@ -1,7 +1,7 @@
 from utility.predict.data import FILE_NAME as FILE_NAME
 from utility.predict.data import output_file_classifier as output_file_classifier
 
-__all__ [
+__all__ = [
     "FILE_NAME"
     , "output_file_classifier"
 ]
