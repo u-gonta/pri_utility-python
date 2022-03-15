cc0780f816eedd3f3ed17519901fc8f84b5da089
唐沢 一成 <kazushige-karasawa@tenryuseiki.com>
2022-03-15 15:27:50 +0900

    予測値の算出を修正

--- utility/tuning/__init__.py
+++ utility/tuning/__init__.py
@@ -1,7 +1,7 @@
 from utility.tuning.data import FILE_NAME as FILE_NAME
 from utility.tuning.data import optuna_classifier as optuna_classifier
 
-__all__ = [
+__all__ [
     "FILE_NAME"
     , "optuna_classifier"
 ]
